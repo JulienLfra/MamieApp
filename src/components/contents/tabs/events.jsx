b9255ff58dcffbb5e04fa072478a688120f7b124
Guillaume Bozon <guigui_le_lionnais@hotmail.fr>
2020-01-09 11:21:39 +0100

    [1.5.0] Refacto de la page Events

--- src/components/contents/tabs/events.jsx
+++ src/components/contents/tabs/events.jsx
@@ -25,7 +25,7 @@ class Events extends React.Component {
             <div className=" col">
               <Card className=" shadow">
                 <CardHeader className="bg-transparent">
-                  <Row className="align-items-center">
+                  <Row>
                     <Col xs="8">
                       <h3 className="mb-0">My events</h3>
                     </Col>
@@ -42,161 +42,72 @@ class Events extends React.Component {
                   </Row>
                 </CardHeader>
                 <CardBody>
-                  <Row className=" icon-examples">
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          id="tooltip982655500"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Fête de la bière</span>
-                          </div>
-                        </button>
+                  <Row>
+                    <Col lg="4" md="6">
+                      <Card className="bg-default shadow">
+                        <CardHeader className="bg-transparent border-0">
+                          <Row className="align-items-center">
+                            <Col xs="8">
+                              <h4 className="text-white mb-0">Fête de la bière</h4>
+                            </Col>
+                            <Col className="text-right" xs="4">
+                              <h6 className="text-white mb-0">10-04-2019</h6>
+                            </Col>
+                          </Row>
+                        </CardHeader>
+                        <CardBody>
+                          <Card className="bg-default shadow">
+                            <img
+                              alt="..."
+                              src={require("../../../assets/img/theme/events-card.jpg")}
+                            />
+                          </Card>
+                        </CardBody>
+                      </Card>
                     </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="air-baloon"
-                          id="tooltip47550434"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Noel</span>
-                          </div>
-                        </button>
+                    <Col lg="4" md="6">
+                      <Card className="bg-default shadow">
+                          <CardHeader className="bg-transparent border-0">
+                            <Row className="align-items-center">
+                              <Col xs="8">
+                                <h4 className="text-white mb-0">Reveillon</h4>
+                              </Col>
+                              <Col className="text-right" xs="4">
+                                <h6 className="text-white mb-0">24-09-2019</h6>
+                              </Col>
+                            </Row>
+                          </CardHeader>
+                          <CardBody>
+                            <Card className="bg-default shadow">
+                              <img
+                                alt="..."
+                                src={require("../../../assets/img/theme/events-card.jpg")}
+                              />
+                            </Card>
+                          </CardBody>
+                        </Card>
                     </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="album-2"
-                          id="tooltip945481346"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Jour de l'an</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="align-center"
-                          id="tooltip662352101"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Fête des lumières</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="align-left-2"
-                          id="tooltip125499785"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Vacance</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="ambulance"
-                          id="tooltip382136785"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Anniversaire Didier</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="app"
-                          id="tooltip3354607"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Cousinade</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="archive-2"
-                          id="tooltip949558633"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Fête de Sylvain</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="atom"
-                          id="tooltip742747005"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Tennis</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="badge"
-                          id="tooltip488565068"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Randonnée</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="bag-17"
-                          id="tooltip163626790"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Soirée</span>
-                          </div>
-                        </button>
-                    </Col>
-                    <Col lg="3" md="6">
-                        <button
-                          className=" btn-icon-clipboard"
-                          data-clipboard-text="basket"
-                          id="tooltip387253692"
-                          type="button"
-                        >
-                          <div>
-                            <i className="ni ni-square-pin text-red" />
-                            <span>Reveillon</span>
-                          </div>
-                        </button>
+                    <Col lg="4" md="6">
+                      <Card className="bg-default shadow">
+                          <CardHeader className="bg-transparent border-0">
+                            <Row className="align-items-center">
+                              <Col xs="8">
+                                <h4 className="text-white mb-0">Anniversaire Didier</h4>
+                              </Col>
+                              <Col className="text-right" xs="4">
+                                <h6 className="text-white mb-0">31-08-2019</h6>
+                              </Col>
+                            </Row>
+                          </CardHeader>
+                          <CardBody>
+                            <Card className="bg-default shadow">
+                              <img
+                                alt="..."
+                                src={require("../../../assets/img/theme/events-card.jpg")}
+                              />
+                            </Card>
+                          </CardBody>
+                        </Card>
                     </Col>
                   </Row>
                 </CardBody>
